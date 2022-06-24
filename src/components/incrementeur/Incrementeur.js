ecd684adf7fead338318e6c2a0e71bfabcfe8879
alexsenga <alexxngabo@gmail.com>
2022-06-24 16:29:30 +0200

    style: incrementeur page

--- src/components/incrementeur/Incrementeur.js
+++ src/components/incrementeur/Incrementeur.js
@@ -1,8 +1,25 @@
-import React from 'react'
+import React from 'react';
+import './incrementeur.css';
 
 function Incrementeur() {
   return (
-    <div>Incrementeur</div>
+    <div className="container">
+        <div className="header">
+            <h1>Incrementeur</h1>
+        </div>
+        <main className="main">
+            <h5>Increment / Decrement</h5>
+            <div className='elements'>
+                <div>
+                    <h1>2</h1>
+                </div>
+                <div className="buttons">
+                    <input type="button" className="btn increment" value="INCREMENTER" />
+                    <input type="button" className="btn decrement" value="DECREMENTER" />
+                </div>
+            </div>
+        </main>
+    </div>
   )
 }
 
