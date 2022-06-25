0b2199c13bbce345058d303fe09ec751095038e9
alexsenga <alexxngabo@gmail.com>
2022-06-25 19:18:13 +0200

    chore: install @mui/material

--- src/components/clients/Client.js
+++ src/components/clients/Client.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from 'react';
+import './client.css';
 
 function Client() {
   return (
-    <div>Client</div>
+    <div className="container">
+        <div className="header">
+            <h1>Clients</h1>
+        </div>
+        <main>
+            <section className='section-flex'>
+              <article className='articles'>
+                <form>
+                  <input type="text" placeholder="Entrer le nom du client" />
+                  <input type="text" placeholder="Entrer le nom du client" />
+                  <input type="text" placeholder="Entrer le nom du client" />
+                  <input type="submit" className='submit-btn' value="Ajouter" />
+                </form>
+              </article>
+              <article className='articles'>
+                ok
+              </article>
+            </section>
+        </main>
+    </div>
   )
 }
 
-export default Client
+export default Client;
